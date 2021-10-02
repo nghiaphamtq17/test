55c2bd4bdddd3c746fe46a676164e662ed99645c
NghiaPhamtq17 <88359467+nghiaphamtq17@users.noreply.github.com>
2021-10-02 09:05:14 +0700

    update

--- bai2/index.js
+++ bai2/index.js
@@ -1,16 +1,16 @@
-let matrix = [
+let arr = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 11],
 ];
 
-const length = matrix.length;
+const length = arr.length;
 let count1 = 0,
   count2 = 0;
 
 for (let i = 0; i < length; i++) {
-  count1 += matrix[i][i];
-  count2 += matrix[length - 1 - i][i];
+  count1 += arr[i][i];
+  count2 += arr[length - 1 - i][i];
 }
 console.log(count1);
 console.log(count2);
